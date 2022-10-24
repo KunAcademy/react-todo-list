2b57ac1844382415f305c72c8024d50e2e340cd7
VietHq-Relipa <viethq@relipasoft.com>
2022-10-24 16:08:54 +0700

    Start branch commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
-import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='todo-app'>
-      <TodoList />
+      Hello this is todolist project
     </div>
   );
 }
